78434a7b9a100d46706148006c972aba4d4e1350
dtrungg <trungtm202@gmail.com>
2024-10-15 20:20:05 +0700

    migrate to 17

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,36 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  Provider,
+  importProvidersFrom,
+} from '@angular/core';
+import { RouterModule, provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token.interceptor';
+import { adminRoutes } from './components/admin/admin-routes';
+
+const tokenInterceptorProvider: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: TokenInterceptor,
+  multi: true,
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    //importProvidersFrom(RouterModule.forRoot(routes)),
+    importProvidersFrom(RouterModule.forChild(adminRoutes)),
+    provideHttpClient(withFetch()),
+    //provideHttpClient(),
+    tokenInterceptorProvider,
+    provideClientHydration(),
+    importProvidersFrom(HttpClientModule),
+  ],
 };
